2915402083c8cf4e6836178dcb3ef481281fef2e
Kenil Patel <kenilpatel.svmr@gmail.com>
2026-04-14 01:16:51 +0530

    feat: update API_BASE_URL configuration for production and development environments

--- Frontend/react-app/src/config.js
+++ Frontend/react-app/src/config.js
@@ -1,4 +1,8 @@
 export const CONFIG = {
-  API_BASE_URL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8011',
+  API_BASE_URL:
+    import.meta.env.VITE_API_BASE_URL ||
+    (import.meta.env.PROD
+      ? 'https://linkit-30hi.onrender.com'
+      : 'http://localhost:8011'),
 };
 
